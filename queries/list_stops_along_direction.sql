9a4cb2fbb9750e34c4221c63fc85d3d21942bda2
Vincent <10055205+gel5555@users.noreply.github.com>
2022-02-19 14:17:13 -0500

    Fixed whitespace formating.

--- queries/list_stops_along_direction.sql
+++ queries/list_stops_along_direction.sql
@@ -1,16 +1,16 @@
 /*List all stops in order for a given route direction.*/
 SET @direction_tag := '329_0_329';
 
-SELECT routes.tag 					AS route_tag,
-	   routes.title 				AS route_title,
-       directions.tag 				AS direction_tag,
-       directions.title 			AS direction_title,
-       directions.name 				AS heading,
-       stops.tag 					AS stop_tag,
-       stops.lat					AS stop_lat,
-       stops.lon					AS stop_lon,
-       stops.title 					AS stop_title,
-       stops.stop_along_direction 	AS stop_order
+SELECT routes.tag                   AS route_tag,
+	     routes.title                 AS route_title,
+       directions.tag               AS direction_tag,
+       directions.title             AS direction_title,
+       directions.name              AS heading,
+       stops.tag                    AS stop_tag,
+       stops.lat                    AS stop_lat,
+       stops.lon                    AS stop_lon,
+       stops.title                  AS stop_title,
+       stops.stop_along_direction   AS stop_order
   FROM TTC.routes routes
   LEFT JOIN TTC.directions directions ON directions.route_tag = routes.tag
   LEFT JOIN TTC.stops stops ON stops.route_tag = routes.tag AND stops.direction_tag = directions.tag
