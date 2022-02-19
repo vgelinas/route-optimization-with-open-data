190b5b0e4b96788c91865015e107e841ad31ba96
Vincent <10055205+gel5555@users.noreply.github.com>
2022-02-19 14:15:22 -0500

    Fixed whitespace formating.

--- queries/get_vehicle_trip_history.sql
+++ queries/get_vehicle_trip_history.sql
@@ -3,7 +3,7 @@ SET @vehicle_id := '1096';
 SET @report_date := DATE('2022-02-11');
 
 SELECT loc.id AS vehicle_id,
-	   loc.route_tag,
+	     loc.route_tag,
        loc.direction_tag,
        dir.title AS direction_title,
        dir.name AS towards,
@@ -13,6 +13,7 @@ SELECT loc.id AS vehicle_id,
        loc.lon,
        DATE_FORMAT(loc.read_time, "%a %d") AS day,
        DATE_FORMAT(loc.read_time, "%T") AS time
+
   FROM TTC.vehicle_locations loc 
   LEFT JOIN TTC.directions dir ON dir.tag = loc.direction_tag  
  WHERE loc.id = @vehicle_id
