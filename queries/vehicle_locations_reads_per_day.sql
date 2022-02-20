aac5ddbbe1a9fd42255615b0292d41133042c99c
Vincent <10055205+gel5555@users.noreply.github.com>
2022-02-20 13:56:30 -0500

    Added day of week column.

--- queries/vehicle_locations_reads_per_day.sql
+++ queries/vehicle_locations_reads_per_day.sql
@@ -1,11 +1,12 @@
 /*Get the daily number of vehicles seen and the number of sensor reads taken over the last month.*/
 
-SELECT DATE(read_time)                                 AS date, 
+SELECT DATE(read_time)                                 AS date,
+       DATE_FORMAT(read_time, "%a")                    AS weekday,
        COUNT(DISTINCT id)                              AS vehicles_seen,
        COUNT(1)                                        AS reads_taken,
        SUM(COUNT(1)) OVER (ORDER BY DATE(read_time))   AS cumul_reads
 
   FROM TTC.vehicle_locations
  WHERE DATE(read_time) >= SUBDATE(CURRENT_DATE(), INTERVAL 1 MONTH)
- GROUP BY 1
- ORDER BY 1;
+ GROUP BY 1,2
+ ORDER BY 1,2;
