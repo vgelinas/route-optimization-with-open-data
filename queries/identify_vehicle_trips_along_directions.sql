9fb389fdf396a6df265452987eafa8f78c1595bf
Vincent <10055205+gel5555@users.noreply.github.com>
2022-02-23 21:33:12 -0500

    Added filter for isolated errors in direction_tag column.

--- queries/identify_vehicle_trips_along_directions.sql
+++ queries/identify_vehicle_trips_along_directions.sql
@@ -15,18 +15,18 @@ SET @num_days = 7;     -- days' worth of data including today
 SET @threshold = 900;  -- 15min in seconds
 
 WITH dir_tag_filter AS (  
-	SELECT id AS vehicle_id,
-		   read_time,
+    SELECT id AS vehicle_id,
+           read_time,
            CASE /*Correct isolated errors in direction_tag column, when tag flips randomly in the middle of a steady group.*/
-		       WHEN LAG(direction_tag) OVER (PARTITION BY id ORDER BY read_time) = LEAD(direction_tag) OVER (PARTITION BY id ORDER BY read_time) 
-			   THEN LAG(direction_tag) OVER (PARTITION BY id ORDER BY read_time)  
-			   ELSE direction_tag 
-		    END AS direction_tag
-	  FROM TTC.vehicle_locations 
-	 WHERE DATE(read_time) >= SUBDATE(CURRENT_DATE(), INTERVAL (@num_days - 1) DAY)
+               WHEN LAG(direction_tag) OVER (PARTITION BY id ORDER BY read_time) = LEAD(direction_tag) OVER (PARTITION BY id ORDER BY read_time) 
+               THEN LAG(direction_tag) OVER (PARTITION BY id ORDER BY read_time)  
+               ELSE direction_tag 
+            END AS direction_tag
+      FROM TTC.vehicle_locations 
+     WHERE DATE(read_time) >= SUBDATE(CURRENT_DATE(), INTERVAL (@num_days - 1) DAY)
      ORDER BY vehicle_id, read_time
 ),
-	 base AS (
+     base AS (
     SELECT vehicle_id,
            direction_tag,
            read_time, 
@@ -53,7 +53,7 @@ WITH dir_tag_filter AS (
      base_fill_helper AS (
     SELECT *, SUM(CASE WHEN trip_number IS NOT NULL THEN 1 ELSE 0 END) OVER (PARTITION BY vehicle_id ORDER BY read_time) AS row_group
       FROM base_with_starts
-	 ORDER BY vehicle_id, read_time
+     ORDER BY vehicle_id, read_time
 ),
      trips AS (
     SELECT vehicle_id, direction_tag, read_time, 
