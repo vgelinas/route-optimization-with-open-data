504408a10d149f08b835e6197dfa92bd22ec65cf
Vincent <10055205+gel5555@users.noreply.github.com>
2022-03-19 19:27:45 -0400

    Fixed tab whitespace issue.

--- queries/segment_vehicle_locations_into_trips.sql
+++ queries/segment_vehicle_locations_into_trips.sql
@@ -25,12 +25,12 @@ to properly identify trip boundaries. See 'offset' arg below.
 
 -----------
 Args: 
-	- left:				Lower bound for trip's last timestamp. Must have
-						timestamp >= left for the trip to be considered. 
-    - right: 			Upper bound for trip's last timestamp. Must have
-						timestamp < right for the trip to be considered. 
-    - offset: 			Number of hours removed from 'left' and added to 
-						'right' when fetching timestamps to create trips.
+    - left:                Lower bound for trip's last timestamp. Must have
+                        timestamp >= left for the trip to be considered. 
+    - right:             Upper bound for trip's last timestamp. Must have
+                        timestamp < right for the trip to be considered. 
+    - offset:             Number of hours removed from 'left' and added to 
+                        'right' when fetching timestamps to create trips.
                         E.g. when querying for 12:00-18:00 with offset=3,
                         we read all timestamps between 9:00-21:00. This 
                         insures that trip boundaries are identified 
@@ -43,81 +43,81 @@ Args:
 
 -----------
 Return columns:
-	- vehicle_id:		unique vehicle identifier
-    - direction_tag: 	unique direction identifier
-    - lat:				latitude
-    - lon:				longitude
-    - read_time:		vehicle reading timestamp
-    - trip_id:			trip unique identifier (timestamp at trip start)
+    - vehicle_id:        unique vehicle identifier
+    - direction_tag:     unique direction identifier
+    - lat:                latitude
+    - lon:                longitude
+    - read_time:        vehicle reading timestamp
+    - trip_id:            trip unique identifier (timestamp at trip start)
 
 */
 
-WITH preprocessed_base AS (		/*Pull data for the wanted period (with offset), and fix isolated sensor errors in the direction_tag.*/
-	SELECT id AS vehicle_id,
-		   CASE WHEN LAG(direction_tag) OVER (PARTITION BY id ORDER BY read_time) = LEAD(direction_tag) OVER (PARTITION BY id ORDER BY read_time) 
+WITH preprocessed_base AS (        /*Pull data for the wanted period (with offset), and fix isolated sensor errors in the direction_tag.*/
+    SELECT id AS vehicle_id,
+           CASE WHEN LAG(direction_tag) OVER (PARTITION BY id ORDER BY read_time) = LEAD(direction_tag) OVER (PARTITION BY id ORDER BY read_time) 
                 THEN LAG(direction_tag) OVER (PARTITION BY id ORDER BY read_time)  
                 ELSE direction_tag 
             END AS direction_tag,  /*Fixes random missing direction_tag in the middle of a trip, or places where the tag randomly flips (e.g. from east to west).*/
-		   lat, 
+           lat, 
            lon, 
            read_time
            
-	  FROM TTC.vehicle_locations
-	 WHERE read_time >= DATE_SUB(%(left)s, INTERVAL %(offset)s HOUR)
+      FROM TTC.vehicle_locations
+     WHERE read_time >= DATE_SUB(%(left)s, INTERVAL %(offset)s HOUR)
        AND read_time < DATE_ADD(%(right)s, INTERVAL %(offset)s HOUR)
-	 ORDER BY vehicle_id, read_time
+     ORDER BY vehicle_id, read_time
 ),
-	 time_to_prev_ts AS (		/*For each vehicle, calculate time to previous timestamp seen on the same direction.*/
-	SELECT vehicle_id,
-		   direction_tag,
-		   read_time, 
+     time_to_prev_ts AS (        /*For each vehicle, calculate time to previous timestamp seen on the same direction.*/
+    SELECT vehicle_id,
+           direction_tag,
+           read_time, 
            TIMESTAMPDIFF(SECOND, 
-				LAG(read_time) OVER (PARTITION BY vehicle_id, direction_tag ORDER BY read_time),
+                LAG(read_time) OVER (PARTITION BY vehicle_id, direction_tag ORDER BY read_time),
                 read_time
                 ) AS sec_to_prev_ts
                 
-	  FROM preprocessed_base
-	 WHERE direction_tag <> 'None'  -- TODO: Fix null insertion issues in ORM, then change this part of the query. 
+      FROM preprocessed_base
+     WHERE direction_tag <> 'None'  -- TODO: Fix null insertion issues in ORM, then change this part of the query. 
 ),
-     start_times AS (			/*Identify trip segments by inspecting the sec_to_prev_ts field for large deviations.*/
-	SELECT vehicle_id, 
-		   direction_tag,
-		   read_time,
+     start_times AS (            /*Identify trip segments by inspecting the sec_to_prev_ts field for large deviations.*/
+    SELECT vehicle_id, 
+           direction_tag,
+           read_time,
            ROW_NUMBER() OVER (PARTITION BY vehicle_id ORDER BY read_time) AS trip_number
            
-	  FROM time_to_prev_ts
-	 WHERE sec_to_prev_ts IS NULL OR sec_to_prev_ts >= 600  -- 10min in seconds, used as threshold
+      FROM time_to_prev_ts
+     WHERE sec_to_prev_ts IS NULL OR sec_to_prev_ts >= 600  -- 10min in seconds, used as threshold
 ),
-     loc_data_with_starts AS ( 	/*Left join start_times to the location data, tagging all trip starts with a trip_id.*/
-	SELECT base.*, 
-		   start_times.trip_number
+     loc_data_with_starts AS (     /*Left join start_times to the location data, tagging all trip starts with a trip_id.*/
+    SELECT base.*, 
+           start_times.trip_number
            
-	  FROM preprocessed_base base 
-	  LEFT JOIN start_times ON base.vehicle_id=start_times.vehicle_id 
-							AND base.direction_tag=start_times.direction_tag 
-							AND base.read_time=start_times.read_time
+      FROM preprocessed_base base 
+      LEFT JOIN start_times ON base.vehicle_id=start_times.vehicle_id 
+                            AND base.direction_tag=start_times.direction_tag 
+                            AND base.read_time=start_times.read_time
 ),
-	 loc_data_fill_helper AS ( 	/*Create a row_group column, identifying which rows belong to the same trip.*/
-	SELECT *,
-		   SUM(CASE WHEN trip_number IS NULL THEN 0 ELSE 1 END) OVER (PARTITION BY vehicle_id ORDER BY read_time) AS row_group
+     loc_data_fill_helper AS (     /*Create a row_group column, identifying which rows belong to the same trip.*/
+    SELECT *,
+           SUM(CASE WHEN trip_number IS NULL THEN 0 ELSE 1 END) OVER (PARTITION BY vehicle_id ORDER BY read_time) AS row_group
            
-	  FROM loc_data_with_starts
+      FROM loc_data_with_starts
 ),
-	 loc_data_with_trips AS (	/*Use row_group to create trip_start and trip_end time columns, constant over each trip.*/
-	SELECT vehicle_id,
-		   direction_tag,
+     loc_data_with_trips AS (    /*Use row_group to create trip_start and trip_end time columns, constant over each trip.*/
+    SELECT vehicle_id,
+           direction_tag,
            lat, 
            lon, 
            read_time, 
            MIN(read_time) OVER (PARTITION BY vehicle_id, row_group) AS trip_start,
            MAX(read_time) OVER (PARTITION BY vehicle_id, row_group) AS trip_end  
            
-	  FROM loc_data_fill_helper
+      FROM loc_data_fill_helper
 )
 
 
 SELECT vehicle_id,
-	   direction_tag, 
+       direction_tag, 
        lat, 
        lon, 
        read_time, 
