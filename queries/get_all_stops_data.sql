c9547a45996f2e41e1f6485f644b47f18953faf8
Vincent <10055205+gel5555@users.noreply.github.com>
2022-03-19 21:31:33 -0400

    Fixed tab whitespace issue.

--- queries/get_all_stops_data.sql
+++ queries/get_all_stops_data.sql
@@ -2,10 +2,10 @@
 This is used for predicting vehicle times at each stops, joining it to the trips table.*/
 
 SELECT directions.tag               AS direction_tag,
-	   stops.tag                    AS stop_tag,
-	   stops.lat                    AS lat,
-	   stops.lon                    AS lon,
-	   stops.stop_along_direction   AS stop_order
+       stops.tag                    AS stop_tag,
+       stops.lat                    AS lat,
+       stops.lon                    AS lon,
+       stops.stop_along_direction   AS stop_order
   FROM TTC.directions directions
   LEFT JOIN TTC.stops stops ON stops.direction_tag = directions.tag
   ORDER BY direction_tag, stop_order
